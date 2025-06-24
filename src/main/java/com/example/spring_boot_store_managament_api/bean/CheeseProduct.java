3c75dad8b1bb0b426347a69a497107b7afc9bc1b
Melania RACOLȚA <melania.racolta@stud.acs.upb.ro>
2025-06-24 22:04:25 +0300

    Testing Lombok's @Builder
      Basic example with @Builder annotation
    
      /test

--- src/main/java/com/example/spring_boot_store_managament_api/bean/CheeseProduct.java
+++ src/main/java/com/example/spring_boot_store_managament_api/bean/CheeseProduct.java
@@ -1,27 +1,38 @@
 package com.example.spring_boot_store_managament_api.bean;
 
+import lombok.*;
+
+//@Getter
+//@Setter
+//@EqualsAndHashCode
+//@NoArgsConstructor(force = true)
+// @RequiredArgsConstructor
+//@AllArgsConstructor
+//@ToString(includeFieldNames = false)
+// kept some of the above annotations for my particular examples, otherwise error on constructor because Data generates only the Requested constructor
+//@Data
+
+@Builder
+@ToString
 public class CheeseProduct {
 
-    private String cheese;
+    // getter/setter for a certain field
+    // by default, lombok generates the getter/setter with 'public' modifier
+//    @Getter(AccessLevel.PRIVATE)
+//    @Setter(AccessLevel.PROTECTED)
+    //@EqualsAndHashCode.Exclude()
+    //@NonNull
+    private final String cheese;
+    // @ToString.Exclude
     private int price;
 
-    public CheeseProduct(String cheese, int price) {
-        this.cheese = cheese;
-        this.price = price;
+    public static void main(String[] args) {
+        CheeseProduct cheeseProduct = new CheeseProduct.CheeseProductBuilder()
+                .cheese("Chavignol")
+                .price(24)
+                .build();
+
+        System.out.println(cheeseProduct);
     }
 
-    public String getCheeseName() {
-        return cheese;
-    }
-
-    public void setCheeseName(String cheese) { this.cheese = cheese;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
 }
